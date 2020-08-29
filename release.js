9f34cbea4c4a64647d23a5c672d9b3737609112c
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2020-08-29 14:33:15 +0200

    lightweight release

---
+++ release.js
@@ -0,0 +1,49 @@
+require('dotenv').config()
+const path = require('path')
+const fs = require('fs')
+const Octokit = require('@octokit/rest').Octokit
+
+const pkg = require('./package.json')
+
+const octokit = new Octokit({ auth: `token ${process.env.GITHUB_TOKEN}` })
+
+const [ , owner, repo ] = pkg.repository.url.match(/^https:\/\/github.com\/([^\/]+)/([^.]+).git$/)
+const xpi = pkg.name.replace('zotero-', '') + '-' + pkg.version + '.xpi'
+
+(async function() {
+  // create release and attach xpi
+  let release = await octokit.repos.createRelease({ owner, repo, tag_name: `v${pkg.version}`, body: ''})
+
+  await octokit.repos.uploadReleaseAsset({
+    owner,
+    repo,
+    url: release.data.upload_url,
+    release_id: release.data.id,
+    data: fs.readFileSync(xpi),
+    headers: {
+      'content-type': 'application/vnd.zotero.plugin',
+      'content-length': fs.statSync(xpi).size,
+    },
+    xpi,
+  })
+
+  release = await octokit.repos.getReleaseByTag({ owner, repo, 'release' })
+
+  const assets = (await octokit.repos.listReleaseAssets({ owner, repo, release_id: release.data.id })).data
+  const update_rdf = assets.find(asset => asset.name === 'update.rdf')
+  if (update_rdf) await octokit.repos.deleteReleaseAsset({ owner, repo, asset_id: update_rdf.id })
+
+  await octokit.repos.uploadReleaseAsset({
+    owner,
+    repo,
+    url: release.data.upload_url,
+    release_id: release.data.id,
+    data: fs.readFileSync('update.rdf'),
+    headers: {
+      'content-type': 'application/rdf+xml',
+      'content-length': fs.statSync('update.rdf').size,
+    },
+    'update.rdf',
+  })
+
+})().catch(err => throw err)
