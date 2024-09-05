c58ed2dbe42fdf33ba8ab82b47a174a39865837a
Emiliano Heyns <emiliano.heyns@iris-advies.com>
2024-09-05 16:04:04 +0200

    cleanup

--- client.ts
+++ client.ts
@@ -1,5 +1,5 @@
 declare const Zotero: any
 
 export const is7 = Zotero.platformMajorVersion >= 102
-export const platform = ['Win', 'Mac', 'Linux'].find(n => Zotero[`is${n}`]) || 'Unknown',
+export const platform = ['Win', 'Mac', 'Linux'].find(n => Zotero[`is${n}`]) || 'Unknown'
 export const client = Zotero.clientName.toLowerCase().replace('-', '')
